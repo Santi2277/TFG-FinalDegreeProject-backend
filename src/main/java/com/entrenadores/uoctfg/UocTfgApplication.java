d012f9f628d84e61e9b728f914ed476faab72d23
slozanocas <slozanocas@uoc.edu>
2021-02-27 23:41:07 +0100

    Initial Commit: Spring Boot Starters + application.properties datasource

---
+++ src/main/java/com/entrenadores/uoctfg/UocTfgApplication.java
@@ -0,0 +1,13 @@
+package com.entrenadores.uoctfg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UocTfgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UocTfgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/entrenadores/uoctfg/UocTfgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entrenadores.uoctfg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UocTfgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
